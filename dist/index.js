866bb1912f449b8e9a66a83eda62d2fca145864c
George Michael <george.michael@unicaf.org>
2023-12-01 15:42:56 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -28926,6 +28926,7 @@ async function run() {
 
     console.log(github.context.repo)
     console.log(github.context.issue.number)
+    console.log(core)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
