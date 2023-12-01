8bf4cd339aa41f3375982a98d695ed51bfad4179
George Michael <george.michael@unicaf.org>
2023-12-01 15:40:55 +0200

    test

--- src/main.js
+++ src/main.js
@@ -13,8 +13,7 @@ async function run() {
     const octokit = github.getOctokit(GITHUB_TOKEN)
 
     console.log(github.context.repo)
-    console.log(github.context.issue)
-    console.log(github.context.labels)
+    console.log(github.context.issue.number)
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
